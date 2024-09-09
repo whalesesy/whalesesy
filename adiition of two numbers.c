c8136358ecc5b6c6e18bb2f97c9f1905e031cb7e
celestine nyoka bwari <165144529+whalesesy@users.noreply.github.com>
2024-09-09 12:45:59 +0300

    Add files via upload

---
+++ adiition of two numbers.c
@@ -0,0 +1,16 @@
+// program to compute the sum of two integers 
+# include <stdio.h>
+int main()
+{
+	int x,y , sum ;
+	//enter value one
+	printf("enter the value of x:");
+	scanf("%d",&x);
+	//enter the value of two 
+	printf("enter the value of y:");
+	scanf("%d",& y);
+	sum = x+y;
+	printf("the sum is",sum);
+	
+	return sum;
+}
